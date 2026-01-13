5019bbb279191e1648e9920c21b416b2377fb7a5
Mark Tristan C. Garcia <147889724+Tristan8100@users.noreply.github.com>
2026-01-13 10:33:26 +0800

    staged for deployment

--- lib/api.ts
+++ lib/api.ts
@@ -1,10 +1,32 @@
 
 import axios from 'axios';
+import router from 'next/router';
+import { useEffect } from 'react';
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:4000', // Laravel or any backend
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // Laravel or any backend
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
   },
+});
+
+export const api2 = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+});
+
+api2.interceptors.request.use((config) => {
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
+  return config;
 });
